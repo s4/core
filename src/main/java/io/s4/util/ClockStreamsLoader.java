003706cfb0de647ebd027a52a6ce210064a3c8b5
Suraj Waghulde <surajwaghulde@yahoo.com>
2010-11-17 14:54:18 -0800

    Added s4 clock to MainApp and the s4_start script. AbstractPE changed to use s4 clock. PEContainer is changed to use update time on each event received. ClockStreamsLoader is added to load streams from applications to s4 clock. EventClock is updated to handle streams and update time.

---
+++ src/main/java/io/s4/util/ClockStreamsLoader.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2010 Yahoo! Inc. All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License. See accompanying LICENSE file. 
+ */
+
+package io.s4.util;
+
+import java.util.HashMap;
+
+import org.apache.log4j.Logger;
+
+public class ClockStreamsLoader {
+
+    private static Logger logger = Logger.getLogger(ClockStreamsLoader.class);
+    Clock s4clock;
+    HashMap<String, String> streamFieldMap;
+
+    public void setStreamFieldMap(HashMap<String, String> streamFieldMap) {
+        this.streamFieldMap = streamFieldMap;
+    }
+
+    public void setS4clock(Clock s4clock) {
+        this.s4clock = s4clock;
+    }
+
+    public void addStreams() {
+        System.out.println("Adding application streams to s4 clock");
+        if (s4clock instanceof EventClock) {
+            EventClock eventClock = (EventClock) s4clock;
+            for (String streamName : streamFieldMap.keySet()) {
+                eventClock.addEventClockStream(streamName,
+                        streamFieldMap.get(streamName));
+            }
+        }
+    }
+}
